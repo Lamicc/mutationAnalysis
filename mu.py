400464e38f3f4cae270622375ff2e2b6e9154154
Lamic Chan <lamicccc@gmail.com>
2017-10-04 19:41:07 +0100

    script modified

--- mu.py
+++ mu.py
@@ -6,10 +6,39 @@ parser = MMCIFParser()
 #pdbl = PDBList()
 #pdbl.retrieve_pdb_file('5GL1')
 
+##load a mmcif file
 struct = parser.get_structure('5gl1', '5gl1.cif')
 #mmcif_dict = MMCIF2Dict('5t15.cif')
+
+##get meta data(if any)
 #print struct.header.keys()
 #print struct.header["name"]
 #print struct.header["release_date"]
 #print struct.header["resolution"]
-struct
+
+##get models
+it = struct.get_models()
+models = list(it)
+print models
+
+##get chains
+chains = list(models[0].get_chains())
+print chains
+
+##get residues
+## <Residue VAL het=  resseq=4049 icode= >
+'''for chain in chains:
+    print chain
+    residues = list(chain.get_residues())
+    print len(residues)
+'''
+residues = list(chains[0].get_residues())
+#print residues
+
+
+##get atoms
+atoms = list(residues[0].get_atoms())
+print atoms
+
+##get 3D coordinate
+print atoms[0].get_vector()
