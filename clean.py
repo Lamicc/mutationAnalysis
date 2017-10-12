6ff643385e134385d9c22a028d60a9da5cceab14
Lamic Chan <lamicccc@gmail.com>
2017-10-12 15:02:23 +0100

    update data cleaning script
    drop index while writing to csv file

--- clean.py
+++ clean.py
@@ -15,21 +15,21 @@ for i in df.index:
 #congenital myopathy group
 df_my = df[df['Condition(s)'].str.contains('disease|myopathy')]
 df_my.loc[:,'Label'] = "congenital myopathy"
-li = df_my.Amino_acid.tolist()
-shuffle(li)
-print('+'.join(li))
+#li = df_my.Amino_acid.tolist()
+#shuffle(li)
+#print('+'.join(li))
 
 #malignant hyperthermia group
 temp = df[~df['Condition(s)'].str.contains('disease|myopathy')]
 df_hy = temp[temp['Condition(s)'].str.contains('hyperthermia')]
 df_hy.loc[:,'Label'] = "malignant hyperthermia"
-li = df_hy.Amino_acid.tolist()
-shuffle(li)
-print('+'.join(li))
+#li = df_hy.Amino_acid.tolist()
+#shuffle(li)
+#print('+'.join(li))
 
 newdf = pd.concat([df_my,df_hy])
 newdf = newdf.reset_index(drop=True)
-newdf.to_csv("./dataframe.csv")
+newdf.to_csv("./dataframe.csv",index=False)
 
 #df_my.to_csv("./my.csv")
 #df_hy.to_csv("./hy.csv")
